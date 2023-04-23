19da31307bdb457bed8f7b08bf29dd1c787ffedd
Vladimir Katin <katin.v.v@gmail.com>
2023-04-24 00:59:13 +0300

    Fix minio utils

--- minioUtil.go
+++ minioUtil.go
@@ -2,37 +2,37 @@ package main
 
 import (
 	"context"
+	"log"
 	"mime/multipart"
 	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/minio/minio-go/v7"
 	"github.com/minio/minio-go/v7/pkg/credentials"
 )
 
 // uploadToMinio uploads a file to Minio
 func uploadToMinio(filename string, file multipart.File, size int64) error {
+	// Load environment variables from .env.local file
+	err := godotenv.Load(".env.local")
+	if err != nil {
+		log.Fatal("Error loading .env.local file:", err)
+	}
 
-	// Get Minio access key and secret key from environment variables
 	minioAccessKey := os.Getenv("MINIO_ACCESS_KEY")
 	minioSecretKey := os.Getenv("MINIO_SECRET_KEY")
-	minioBucket := os.Getenv("MINIO_BUCKET")
-
-	// Set up Minio client
 	minioEndpoint := os.Getenv("MINIO_ENDPOINT")
+	minioBucket := os.Getenv("MINIO_BUCKET")
 	minioUseSSL := false
+
 	// Initialize Minio client
-	minioClient, err := minio.New(
-		minioEndpoint,
-		&minio.Options{
-			Creds:  credentials.NewStaticV4(
-				minioAccessKey,
-				minioSecretKey,
-				"",
-			),
+	minioClient, err := minio.New(minioEndpoint, &minio.Options{
+			Creds:  credentials.NewStaticV4(minioAccessKey, minioSecretKey, ""),
 			Secure: minioUseSSL,
 		},
 	)
 	if err != nil {
+		log.Fatal(err)
 		return err
 	}
 
